8531d2e04491ba2b0e5055362334627738142832
Masafuro <apa226@gmail.com>
2023-01-06 14:21:16 +0900

    ファイル取得プログラムfileFinder.pyの完成
    --info オプション指定で取得したファイル一覧を表示

--- dev/fileFinder.py
+++ dev/fileFinder.py
@@ -1,26 +1,60 @@
 import os
-print("START fileFinder.py")
+import pathlib
+from tabulate import tabulate
 
-object_path ="./object"
+import argparse
+parser = argparse.ArgumentParser()
+
+# "--info" 指定でインポートした画像をprintで出力
+parser.add_argument("--info", help="optional", action="store_true")
+args = parser.parse_args()
+
+print("\n")
+print("START fileFinder.py")
+print("\n*************************\n")
+
+object_path ="./trimmed"
+
 files = os.listdir( object_path )
 files_dir = [f for f in files if os.path.isdir(os.path.join( object_path , f))]
 labels = files_dir
 print("labels:", labels)
 
-path = "./object"
+'''
+# 画像一覧を取得
+path = "./object/chips"
 files = os.listdir( path )
 files_file = [f for f in files if os.path.isfile(os.path.join(path, f))]
 print(files_file)   # ['file1', 'file2.txt', 'file3.jpg']
-
-
 '''
+
+maxFileNum = 0
+maxLabelNum = len(labels)
+
 # ラベルフォルダからの画像取得
 for w in labels:
-    print(w + ":" + str(type(w)) )
     img_path = object_path + "/" + w
-    print("img_path:" + img_path + ":" + str(type(img_path)))
-    img_files_path = os.listdir(img_path)
-    img_files = [f for f in img_files_path if os.path.isfile(os.path.join(img_files_path, f))]
-    print(img_files)   # ['file1', 'file2.txt', 'file3.jpg']
+    img_list = list(pathlib.Path( img_path ).glob('**/*.png'))
+    if maxFileNum < len(img_list):
+        maxFileNum = len(img_list)
 
-'''
+# ラベルの総数、ラベルフォルダ内の画像のうち、最大数を取得
+print( "maxFileNum:"+ str(maxFileNum) )
+print( "maxLabelNum:" + str(maxLabelNum) )
+
+img_array = [[0 for i in range( maxFileNum )] for j in range( maxLabelNum )] #配列[ラベル数][最大ファイル数]を宣言
+
+for j in range( maxLabelNum ):
+    # for i in range( maxFileNum ):
+    img_array_path = object_path + "/" + labels[j]
+    img_array[j] = list(pathlib.Path( img_array_path ).glob('**/*.png'))
+
+print( "Images imported." )
+
+if args.info:
+    print( tabulate( img_array ) )
+
+print("\n*************************\n")
+print("END fileFinder.py")
+
+# print(img_array[3][1]) #img_array チェック用
